41027e5d9bdc9890c5bf395c2c60b9462b344a40
Khanhdzudo <89830152+Khanhdzudo@users.noreply.github.com>
2021-10-15 16:23:58 +0930

    Install Webbrowser in App
    Install Web browser in App by Expo

---
+++ appExpo/app_test.js
@@ -0,0 +1,258 @@
+
+/*
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+export default class Profile extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+          <View style={styles.header}></View>
+          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>John Doe</Text>
+              <Text style={styles.info}>UX Designer / Mobile developer</Text>
+              <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
+              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Opcion 1</Text>  
+              </TouchableOpacity>              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Opcion 2</Text> 
+              </TouchableOpacity>
+            </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  header:{
+    backgroundColor: "#00BFFF",
+    height:200,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom:10,
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop:130
+  },
+  name:{
+    fontSize:22,
+    color:"#FFFFFF",
+    fontWeight:'600',
+  },
+  body:{
+    marginTop:40,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: 'center',
+    padding:30,
+  },
+  name:{
+    fontSize:28,
+    color: "#696969",
+    fontWeight: "600"
+  },
+  info:{
+    fontSize:16,
+    color: "#00BFFF",
+    marginTop:10
+  },
+  description:{
+    fontSize:16,
+    color: "#696969",
+    marginTop:10,
+    textAlign: 'center'
+  },
+  buttonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#00BFFF",
+  },
+});
+*/
+
+
+import React from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  FlatList, 
+  Dimensions,
+  TouchableOpacity,
+  Image,
+  Modal,
+  ScrollView
+} from 'react-native';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numColumns:3,
+      userSelected:{},
+      modalVisible:false,
+      data: [
+        {id:1,  image:"https://bootdey.com/img/Content/avatar/avatar1.png"}, 
+        {id:2,  image:"https://bootdey.com/img/Content/avatar/avatar2.png"}, 
+        {id:3,  image:"https://bootdey.com/img/Content/avatar/avatar3.png"}, 
+        {id:4,  image:"https://bootdey.com/img/Content/avatar/avatar4.png"}, 
+        {id:5,  image:"https://bootdey.com/img/Content/avatar/avatar5.png"}, 
+        {id:6,  image:"https://bootdey.com/img/Content/avatar/avatar6.png"}, 
+        {id:7,  image:"https://bootdey.com/img/Content/avatar/avatar7.png"}, 
+        {id:8,  image:"https://bootdey.com/img/Content/avatar/avatar1.png"}, 
+        {id:9,  image:"https://bootdey.com/img/Content/avatar/avatar2.png"}, 
+        {id:10, image:"https://bootdey.com/img/Content/avatar/avatar3.png"},
+      ]
+    };
+  }
+
+  clickEventListener = (item) => {
+    this.setState({userSelected: item}, () =>{
+      this.setModalVisible(true);
+    });
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  renderItem = ({item, index}) => {
+    if (item.empty === true) {
+      return <View style={[styles.item, styles.itemInvisible]} />;
+    }
+    var itemDimension = Dimensions.get('window').width / this.state.numColumns;
+    return (
+      <TouchableOpacity style={[styles.item, {height: itemDimension}]} onPress={() => {this.clickEventListener(item)}}>
+        <Image style={{height:itemDimension - 2, width:itemDimension - 2}} source={{uri: item.image}}/>
+      </TouchableOpacity>
+    );
+  }
+  
+  formatRow = (data, numColumns) => {
+    const numberOfFullRows = Math.floor(data.length / numColumns);
+    let numberOfElementsLastRow = data.length - (numberOfFullRows * numColumns);
+    while (numberOfElementsLastRow !== numColumns && numberOfElementsLastRow !== 0) {
+      data.push({ id: `blank-${numberOfElementsLastRow}`, empty: true });
+      numberOfElementsLastRow++;
+    }
+    return data;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.formatRow(this.state.data, this.state.numColumns)}
+          keyExtractor= {(item) => {
+            return item.id;
+          }}
+          renderItem={this.renderItem}
+          numColumns={this.state.numColumns}/>
+
+        <Modal
+          animationType={'fade'}
+          transparent={true}
+          onRequestClose={() => this.setModalVisible(false)}
+          visible={this.state.modalVisible}>
+
+          <View style={styles.popupOverlay}>
+            <View style={styles.popup}>
+              <View style={styles.popupContent}>
+                <ScrollView contentContainerStyle={styles.modalInfo}>
+                  <Image style={{width:200, height:200}} source={{uri: this.state.userSelected.image}}/>
+                </ScrollView>
+              </View>
+              <View style={styles.popupButtons}>
+                <TouchableOpacity onPress={() => {this.setModalVisible(false) }} style={styles.btnClose}>
+                  <Text style={styles.txtClose}>Close</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginVertical: 20,
+  },
+  item: {
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  itemInvisible: {
+    backgroundColor: 'transparent',
+  },
+
+ /************ modals ************/
+  popup: {
+    backgroundColor: 'white',
+    marginTop: 80,
+    marginHorizontal: 20,
+    borderRadius: 7,
+  },
+  popupOverlay: {
+    backgroundColor: "#00000057",
+    flex: 1,
+    marginTop: 20
+  },
+  popupContent: {
+    //alignItems: 'center',
+    margin: 5,
+    height:250,
+  },
+  popupHeader: {
+    marginBottom: 45
+  },
+  popupButtons: {
+    marginTop: 15,
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderColor: "#eee",
+    justifyContent:'center'
+  },
+  popupButton: {
+    flex: 1,
+    marginVertical: 16
+  },
+  btnClose:{
+    height:20,
+    backgroundColor:'#20b2aa',
+    padding:20
+  },
+  modalInfo:{
+    alignItems:'center',
+    justifyContent:'center',
+  }
+}); 
